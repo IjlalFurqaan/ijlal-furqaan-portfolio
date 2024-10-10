2a597598307eb71280441a32f83694c0bb00c307
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-10-10 15:23:08 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -126,7 +126,7 @@ const Home = () => {
         </Description>
 
         <Button
-          to="/contact" // Change href to 'to' and point to the contact route
+          to="/contact" 
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
           initial={{ opacity: 0, y: 20 }}
